21dbd3413f9408ee5348ad3642cd55b527228e7a
Belurgas <morethenworking@outlook.com>
2025-06-19 22:16:22 +0300

    Delete old version

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("icons/try.ico") // Путь к иконке
-           .compile().unwrap();
-    }
-}
