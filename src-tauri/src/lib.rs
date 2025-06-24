69150227997cb929f0ee5fc922485a52bd64e429
Belurgas <morethenworking@outlook.com>
2025-06-24 16:17:35 +0300

    Add logging tauri plagin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -74,6 +74,7 @@ pub fn run() {
     });
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
