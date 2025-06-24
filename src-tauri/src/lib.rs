a435667691d4105f9de29efd0ab02003b56a2be6
Belurgas <morethenworking@outlook.com>
2025-06-24 17:53:42 +0300

    Create tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, sync::mpsc, thread};
 
 use sysinfo::{Components, Disks, MemoryRefreshKind, System};
-use tauri::{Emitter, Manager};
+use tauri::{menu::{Menu, MenuItem}, tray::TrayIconBuilder, Emitter, Manager};
 use tauri_plugin_positioner::{Position, WindowExt};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -85,6 +85,23 @@ pub fn run() {
             system_info
         ])
         .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+            
+            let tray = TrayIconBuilder::new()
+            .menu(&menu)
+            .show_menu_on_left_click(true)
+            .on_menu_event(|app, event| match event.id.as_ref() {
+                "quit" => {
+                println!("quit menu item was clicked");
+                app.exit(0);
+                }
+                _ => {
+                println!("menu item {:?} not handled", event.id);
+                }
+            })
+            .icon(app.default_window_icon().unwrap().clone()).build(app)?;
+
             let window = app.get_webview_window("main").unwrap();
             let _ = window.as_ref().window().move_window(Position::Center);
             tauri::async_runtime::spawn(async move {
